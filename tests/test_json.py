69e5553e57ba50fc7c04ff6eec6b8b3110f26ad0
Justin Reese <justaddcoffee@gmail.com>
2023-06-15 15:50:05 -0400

    Add 1 trivial test

---
+++ tests/test_json.py
@@ -0,0 +1,12 @@
+import unittest
+from unittest import TestCase
+
+class JsonLDTestCase(TestCase):
+
+    def test_reality(self):
+        reality = True
+        self.assertTrue(reality)
+
+
+if __name__ == '__main__':
+    unittest.main()
