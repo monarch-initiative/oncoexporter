ced0a387516a60681bc0e33ef8cda864d04aab0f
Peter Robinson <peter.robinson@jax.org>
2023-09-23 15:13:38 -0400

    disease object

--- tests/test_op_disease.py
+++ tests/test_op_disease.py
@@ -9,20 +9,29 @@ class OpDiseaseTestCase(TestCase):
 
     @classmethod
     def setUpClass(cls) -> None:
-        data = ["CGCI-HTMCP-CC.HTMCP-03-06-02003.HTMCP-03-06-02003_diagnosis",
-                "[{'system': 'GDC', 'field_name': 'case.diagnoses.diagnosis_id', 'value': '1ffb1fc3-b47f-5934-9927-1a5156e159d8'},{'system': 'GDC', 'field_name': 'case.diagnoses.submitter_id', 'value': 'HTMCP-03-06-02003_diagnosis'}]",
-                "Squamous cell carcinoma, nonkeratinizing, NOS",
-                "NaN",
-                "8072/3",
+        data_1 = ["PDC000220.P065.P065-DX",
+                "[{'system': 'PDC', 'field_name': 'Diagnosis.diagnosis_id', 'value': '24b7022a-8beb-11ea-b1fd-0aad30af8a83'}, {'system': 'PDC', 'field_name': 'Diagnosis.diagnosis_submitter_id', 'value': 'P065-DX'}]",
                 "None",
-                "G2",
-                "Biopsy",
-                "CGCI.HTMCP-03-06-02003",
-                "CGCI-HTMCP-CC.HTMCP-03-06-02003"]
+                "NaN",
+                "None",
+                "None",
+                "None",
+                "None",
+                "Academia Sinica LUAD-100.P065",
+                "PDC000220.P065",
+                "PDC000219.P065",
+                "[{'system': 'PDC', 'field_name': 'Case.case_id', 'value': '24b3ad62-8beb-11ea-b1fd-0aad30af8a83'}, {'system': 'PDC', 'field_name': 'Case.case_submitter_id', 'value': 'P065'}]",
+                 "PDC000219",
+                "None",
+                "Lung"]
         cls._column_names = ['diagnosis_id', 'diagnosis_identifier', 'primary_diagnosis',
                              'age_at_diagnosis', 'morphology', 'stage', 'grade',
-                             'method_of_diagnosis', 'subject_id', 'researchsubject_id']
-        cls._series = pd.Series(data, index=cls._column_names)
+                            'method_of_diagnosis', 'subject_id', 'researchsubject_id_di',
+                            'researchsubject_id_rs', 'researchsubject_identifier',
+                            'member_of_research_project', 'primary_diagnosis_condition',
+                            'primary_diagnosis_site']
+        assert len(data_1) == len(cls._column_names)
+        cls._series = pd.Series(data_1, index=cls._column_names)
 
     def test_creation(self):
         dfact = CdaDiseaseFactory()
