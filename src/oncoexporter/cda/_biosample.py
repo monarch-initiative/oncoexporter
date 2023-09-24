4fb58d5d1d7cf760697af434c9c21c54bfe8e6a1
Sujay Patil <sujaysanjeev.patil@gmail.com>
2023-09-24 07:46:21 -0700

    map primary_disease_type to histological_diagnosis

--- src/oncoexporter/cda/_biosample.py
+++ src/oncoexporter/cda/_biosample.py
@@ -5,6 +5,8 @@ import phenopackets as pp
 
 HOMO_SAPIENS = pp.OntologyClass(id='NCBITaxon:9606', label='Homo sapiens')
 LUNG = pp.OntologyClass(id='UBERON:0002048', label='lung')
+LUNG_ADENOCARCINOMA = pp.OntologyClass(id='NCIT:C3512', label='Lung Adenocarcinoma')
+LUNG_SQUAMOUS_CELL_CARCINOMA = pp.OntologyClass(id='NCIT:C3493', label='Lung Squamous Cell Carcinoma')
 
 """CDA	Phenopacket
 	
@@ -24,7 +26,12 @@ def make_cda_biosample(row: pd.Series) -> pp.Biosample:
     if sampled_tissue is not None:
         biosample.sampled_tissue = sampled_tissue
 
-    biosample.taxonomy = HOMO_SAPIENS
+    biosample.taxonomy.CopyFrom(HOMO_SAPIENS)
+
+    # primary_disease_type -> histological_diagnosis
+    histological_diagnosis = _map_primary_disease_type(row['primary_disease_type'])
+    if histological_diagnosis is not None:
+        biosample.histological_diagnosis.CopyFrom(histological_diagnosis)
 
     return biosample
 
@@ -36,3 +43,16 @@ def _map_anatomical_site(val: typing.Optional[str]) -> typing.Optional[pp.Ontolo
         return LUNG
     else:
         return None
+
+def _map_primary_disease_type(val: typing.Optional[str]=None) -> typing.Optional[pp.OntologyClass]:
+    if val is not None:
+        val = val.lower()
+        if val == 'lung adenocarcinoma':
+            return LUNG_ADENOCARCINOMA
+        elif val == 'lung squamous cell carcinoma':
+            return LUNG_SQUAMOUS_CELL_CARCINOMA
+        else:
+            return None
+    else:
+        return None
+    
