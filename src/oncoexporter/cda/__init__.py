edf07289341a6cf3f34c36f2844bf28a0daebca8
Peter Robinson <peter.robinson@jax.org>
2023-09-24 15:15:56 -0400

    exporyinh table importer

--- src/oncoexporter/cda/__init__.py
+++ src/oncoexporter/cda/__init__.py
@@ -1,9 +1,14 @@
 from .cda_disease_factory import CdaDiseaseFactory
 from .cda_individual_factory import CdaIndividualFactory
 from .cda_mutation_factory import CdaMutationFactory
+from .cda_table_importer import CdaTableImporter
+
+
+__version__ = "0.0.2"
 
 __all__ = [
  "CdaDiseaseFactory",
  "CdaIndividualFactory",
- "CdaMutationFactory"
+ "CdaMutationFactory",
+ "CdaTableImporter"
 ]
