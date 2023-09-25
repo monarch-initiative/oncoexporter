6d3e8553a0f7849d094fda9c1cd533ece7a12a21
Justin Reese <justaddcoffee@gmail.com>
2023-09-25 09:31:28 -0400

    Add lung ingest python script

---
+++ src/run_lung.py
@@ -0,0 +1,5 @@
+from cdapython import Q
+from oncoexporter.cda import CdaTableImporter
+Tsite = Q('primary_diagnosis_site = "%lung%" OR primary_diagnosis_site = "%pulmonary%"')
+table_importer = CdaTableImporter(query_obj=Tsite, use_cache=True)
+p = table_importer.get_ga4gh_phenopackets()
