ff66c1695ce67a01ed868dd6fed8bd4babfee82a
Justin Reese <justaddcoffee@gmail.com>
2023-12-01 14:09:21 -0500

    Add failing tests for sub-stages (IA, IB, IIA, etc)

--- tests/test_cda_disease_factory.py
+++ tests/test_cda_disease_factory.py
@@ -30,26 +30,26 @@ class TestCdaDiseaseFactory(unittest.TestCase):
                          PPKt.OntologyClass)
 
     @parameterized.expand([
-        ('s1', ('NCIT:C27966', 'Stage I')),         # IA == stage I
-        ('s2', ('NCIT:C27966', 'Stage I')),         # IA == stage I
-        ('s3', ('NCIT:C28054', 'Stage II')),        # IIA == stage II
-        ('s4', ('NCIT:C28054', 'Stage II')),        # IIB == stage II
-        ('s5', ('NCIT:C27970', 'Stage III')),       # IIIA == stage III
-        ('s6', ('NCIT:C27970', 'Stage III')),       # IIIB == stage III
+        ('s1', ('NCIT:C27975', 'Stage IA')),         # IA == stage IA
+        ('s2', ('NCIT:C27976', 'Stage IB')),         # IB == stage IB
+        ('s3', ('NCIT:C27967', 'Stage IIA')),        # IIA == stage IIA
+        ('s4', ('NCIT:C27968', 'Stage IIB')),        # IIB == stage IIB
+        ('s5', ('NCIT:C27977', 'Stage IIIA')),       # IIIA == stage IIIA
+        ('s6', ('NCIT:C27978', 'Stage IIIB')),       # IIIB == stage IIIB
         ('s7', ('NCIT:C27971', 'Stage IV')),        # IV == stage IV
-        ('s8', ('NCIT:C27966', 'Stage I')),         # Stage IA == stage I
-        ('s9', ('NCIT:C27966', 'Stage I')),         # Stage IB
-        ('s10', ('NCIT:C28054', 'Stage II')),       # Stage IIA
-        ('s11', ('NCIT:C28054', 'Stage II')),       # Stage IIB
-        ('s12', ('NCIT:C27970', 'Stage III')),      # Stage IIIA
+        ('s8', ('NCIT:C27975', 'Stage IA')),         # Stage IA
+        ('s9', ('NCIT:C27976', 'Stage IB')),         # Stage IB
+        ('s10', ('NCIT:C27967', 'Stage IIA')),       # Stage IIA
+        ('s11', ('NCIT:C27968', 'Stage IIB')),       # Stage IIB
+        ('s12', ('NCIT:C27977', 'Stage IIIA')),      # Stage IIIA
         ('s13', ('NCIT:C92207', 'Stage Unknown')),  # None == Stage Unknown = C92207
         ('s14', ('NCIT:C27966', 'Stage I')),        # Stage I
         ('s15', ('NCIT:C28054', 'Stage II'))        # Stage II
     ])
     def test_correct_stage_parsing(self, subject_id, expected_ncit_ontology):
-        disease_obj = self.factory.to_ga4gh(
-            self.stage_values_test_df[
-                self.stage_values_test_df['subject_id_rs'] == subject_id].iloc[0])
+        this_row = self.stage_values_test_df[
+            self.stage_values_test_df['subject_id_rs'] == subject_id].iloc[0]
+        disease_obj = self.factory.to_ga4gh(this_row)
 
         expected_oc = PPKt.OntologyClass()
         expected_oc.id = expected_ncit_ontology[0]
@@ -60,5 +60,6 @@ class TestCdaDiseaseFactory(unittest.TestCase):
             any([this_term == expected_oc for this_term in disease_obj.disease_stage]),
             msg = f"Expected ontology term {expected_oc.id} {expected_oc.label}\n" +
             f" not found in disease_obj.disease_stage terms:\n" +
-            "\n".join([this_term.id + " " + this_term.label for this_term in disease_obj.disease_stage])
+            "\n".join([this_term.id + " " + this_term.label for this_term in disease_obj.disease_stage]) +
+            f"while parsing this row\n{this_row}"
         )
