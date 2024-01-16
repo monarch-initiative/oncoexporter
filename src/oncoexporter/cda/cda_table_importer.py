e4095fad063e1f8fd9d9b1e3b035e36823164408
Daniel Danis <daniel.gordon.danis@protonmail.com>
2024-01-16 17:29:33 -0500

    Support storing cache files in a separate folder.

--- src/oncoexporter/cda/cda_table_importer.py
+++ src/oncoexporter/cda/cda_table_importer.py
@@ -15,47 +15,53 @@ from tqdm import tqdm
 
 
 class CdaTableImporter(CdaImporter):
-    """This class is the entry point for transforming CDA data into GA4GH Phenopackets. Client code only needs to initialize it with a CDA query and it can return phenopackets with the **get_ga4gh_phenopackets** method. It also returns individual tables for that can be used for testing or visualizing data.
+    """This class is the entry point for transforming CDA data into GA4GH Phenopackets. Client code only needs
+    to initialize it with a CDA query, and it can return phenopackets with the :func:`get_ga4gh_phenopackets`.
+    It also returns individual tables for that can be used for testing or visualizing data.
 
-    The CDA query determines the cohort that will be retrieved from CDA. This class then retrieves data for this cohort in form of pandas DataFrames and extracts data for phenopacket construction using the data in the tables
+    The CDA query determines the cohort that will be retrieved from CDA. This class then retrieves data
+    for this cohort in form of pandas DataFrames and extracts data for phenopacket construction using the data
+    in the tables
 
     :param cohort_name: A user-chosen name for the cohort
     :type cohort_name: str
-    :param query: A query for CDA such as 'primary_diagnosis_site = "Lung"'
-    :type query: str
-    :param query_obj: a query object created by CDA. Note that either this argument or the query (string) argument must be provided, but not both.
-    :type query_obj: Q
+    :param query: a query object created by CDA. Note that either this argument or the query (string) argument must be provided, but not both.
+    :type query: Q
     :param use_cache: if True, cache/retrieve from cache
     :type use_cache: bool
+    :param cache_dir: a `str` with path to the folder to store the cache files
     """
 
-    def __init__(self, cohort_name: str, query: str = None, query_obj: Q = None,
-                 use_cache=False):
-        """Constructor
-        """
-        if query is not None and query_obj is None:
-            self._query = Q(query)
-        elif query_obj is not None and query is None:
-            if not isinstance(query_obj, Q):
-                raise ValueError(f"query_obj argument must be Q.Q object, but instead was {type(query_obj)}")
-            self._query = query_obj
-        else:
-            raise ValueError("Need to pass either query or query_obj argument but not both")
+    def __init__(self, cohort_name: str,
+                 query: Q,
+                 use_cache: bool = False,
+                 cache_dir: typing.Optional[str] = None):
+        if not isinstance(query, Q):
+            raise ValueError(f"query_obj argument must be Q.Q object, but instead was {type(query)}")
+        self._query = query
+
         self._use_cache = use_cache
         self._cohort_name = cohort_name
 
+        if cache_dir is None:
+            self._cache_dir = os.getcwd()
+        else:
+            if not os.path.isdir(cache_dir) or not os.access(cache_dir, os.W_OK):
+                raise ValueError(f'`cache_dir` must be a writable directory: {cache_dir}')
+
     def _get_cda_df(self, callback_fxn, cache_name: str):
-        print(f"Retrieving dataframe {cache_name}")
-        if self._use_cache and os.path.isfile(cache_name):
-            with open(cache_name, 'rb') as cachehandle:
-                print(f"loading cached dataframe from {cache_name}")
+        fpath_cache = os.path.join(self._cache_dir, cache_name)
+        print(f"Retrieving dataframe {fpath_cache}")
+        if self._use_cache and os.path.isfile(fpath_cache):
+            with open(fpath_cache, 'rb') as cachehandle:
+                print(f"loading cached dataframe from {fpath_cache}")
                 individual_df = pickle.load(cachehandle)
         else:
             print(f"calling CDA function")
             individual_df = callback_fxn()
             if self._use_cache:
-                print(f"Creating cached dataframe as {cache_name}")
-                with open(cache_name, 'wb') as f:
+                print(f"Creating cached dataframe as {fpath_cache}")
+                with open(fpath_cache, 'wb') as f:
                     pickle.dump(individual_df, f)
         return individual_df
 
@@ -72,10 +78,8 @@ class CdaTableImporter(CdaImporter):
         :returns: pandas DataFrame that corresponds to the CDA subject table.
         :rtype: pd.DataFrame
         """
-        if self._query is None:
-            raise Exception(f"Could not retrieve subject dataframe because query object was None")
-        callable = lambda: self._query.subject.run(page_size=page_size).get_all().to_dataframe();
-        subject_df = self._get_cda_df(callable, f".{self._cohort_name}_individual_df.pkl");
+        callable = lambda: self._query.subject.run(page_size=page_size).get_all().to_dataframe()
+        subject_df = self._get_cda_df(callable, f"{self._cohort_name}_individual_df.pkl")
         return subject_df
 
 
@@ -86,10 +90,10 @@ class CdaTableImporter(CdaImporter):
         :type page_size: int
         """
         diagnosis_callable = lambda: self._query.diagnosis.run(page_size=page_size).get_all().to_dataframe()
-        diagnosis_df = self._get_cda_df(diagnosis_callable, f".{self._cohort_name}_diagnosis_df.pkl")
+        diagnosis_df = self._get_cda_df(diagnosis_callable, f"{self._cohort_name}_diagnosis_df.pkl")
         print("obtained diagnosis_df")
         rsub_callable = lambda: self._query.researchsubject.run(page_size=page_size).get_all().to_dataframe()
-        rsub_df = self._get_cda_df(rsub_callable, f".{self._cohort_name}_rsub_df.pkl")
+        rsub_df = self._get_cda_df(rsub_callable, f"{self._cohort_name}_rsub_df.pkl")
         print("obtained rsub_df")
         merged_df = pd.merge(diagnosis_df, rsub_df, left_on='researchsubject_id', right_on='researchsubject_id',
                                 suffixes=["_di", "_rs"])
@@ -108,17 +112,17 @@ class CdaTableImporter(CdaImporter):
         :rtype: pd.DataFrame
         """
         specimen_callable = lambda: self._query.specimen.run(page_size=page_size).get_all().to_dataframe()
-        specimen_df = self._get_cda_df(specimen_callable, f".{self._cohort_name}_specimen_df.pkl")
+        specimen_df = self._get_cda_df(specimen_callable, f"{self._cohort_name}_specimen_df.pkl")
         return specimen_df
 
     def get_treatment_df(self, page_size=10000) -> pd.DataFrame:
         treatment_callable = lambda: self._query.treatment.run(page_size=page_size).get_all().to_dataframe()
-        treatment_df = self._get_cda_df(treatment_callable, f".{self._cohort_name}_treatment_df.pkl")
+        treatment_df = self._get_cda_df(treatment_callable, f"{self._cohort_name}_treatment_df.pkl")
         return treatment_df
 
     def get_mutation_df(self, page_size=10000) -> pd.DataFrame:
         mutation_callable = lambda: self._query.mutation.run(page_size=page_size).get_all().to_dataframe()
-        mutation_df = self._get_cda_df(mutation_callable, f".{self._cohort_name}_mutation_df.pkl")
+        mutation_df = self._get_cda_df(mutation_callable, f"{self._cohort_name}_mutation_df.pkl")
         return mutation_df
 
 
