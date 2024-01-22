6edac7833206b6033f1cf9e9706d4d5bfb7eff2c
Daniel Danis <daniel.gordon.danis@protonmail.com>
2024-01-22 11:48:33 -0500

    Fix the only complete `CdaMutationFactory` test.

--- tests/test_op_mutation.py
+++ tests/test_op_mutation.py
@@ -1,12 +1,10 @@
-import pandas as pd
-from unittest import TestCase
-import phenopackets as PPkt
 import os
-from oncoexporter.model import OpMutation
-from oncoexporter.cda import CdaMutationFactory
-## REMOVE LATER
-from google.protobuf.json_format import MessageToJson
 
+import pandas as pd
+import phenopackets as PPkt
+import pytest
+
+from oncoexporter.cda import CdaMutationFactory
 
 TESTDATA_FILENAME = os.path.join(os.path.dirname(__file__), 'data', 'mutation_excerpt.tsv')
 
@@ -40,67 +38,61 @@ def get_vcf_ref(vinterpretation):
     return genome_assembly, ref, alt, pos
 
 
-class OpMutationTestCase(TestCase):
-    @classmethod
-    def setUpClass(cls) -> None:
+class TestOpMutation:
+
+    @pytest.fixture
+    def mutation_factory(self) -> CdaMutationFactory:
+        return CdaMutationFactory()
+
+    @pytest.fixture
+    def a1mi(self) -> pd.Series:
         df = pd.read_csv(TESTDATA_FILENAME, sep="\t")
         # TCGA.TCGA-C5-A1MI -- subject is on the first row
         df_a1mi = df[df['cda_subject_id'].str.contains("TCGA.TCGA-C5-A1MI")]
         assert len(df_a1mi) == 1
-        cls._a1mi = df_a1mi.iloc[0]
+        return df_a1mi.iloc[0]
 
-
-
-    def test_a1mi(self):
-        factory = CdaMutationFactory()
-        vinterpretation = factory.to_ga4gh(self._a1mi)
-        self.assertIsNotNone(vinterpretation)
+    def test_a1mi(self, mutation_factory: CdaMutationFactory,
+                  a1mi: pd.Series):
+        vinterpretation = mutation_factory.to_ga4gh(a1mi)
+        assert vinterpretation is not None
         genome_assembly, ref, alt, pos = get_vcf_ref(vinterpretation=vinterpretation)
-        self.assertEqual("hg38", genome_assembly)
+        assert genome_assembly == "GRCh38"
 
-
-
-    def test_gene(self):
+    def test_gene(self, mutation_factory: CdaMutationFactory):
         d = get_column_dict()
         d['Hugo_Symbol'] = "MFG42"
         d['Entrez_Gene_Id'] = "42"
         series = pd.Series(d)
-        factory = CdaMutationFactory()
-        vinterpretation = factory.to_ga4gh(series)
-        self.assertIsNotNone(vinterpretation)
-        self.assertIsNotNone(vinterpretation.variation_descriptor)
-        vdescriptor = vinterpretation.variation_descriptor
-        self.assertIsNotNone(vdescriptor)
-        self.assertIsNotNone(vdescriptor.gene_context)
-        gcontext = vdescriptor.gene_context
-        self.assertEqual("NCBIGene:42", gcontext.value_id)
-        self.assertEqual("MFG42", gcontext.symbol)
-        json_string = MessageToJson(vinterpretation)
-        print(json_string)
 
-    def test_hgvs(self):
+        vinterpretation = mutation_factory.to_ga4gh(series)
+        assert vinterpretation is not None
+        assert vinterpretation.variation_descriptor is not None
+
+        vdescriptor = vinterpretation.variation_descriptor
+        assert vdescriptor is not None
+        assert vdescriptor.gene_context is not None
+
+        gcontext = vdescriptor.gene_context
+        assert gcontext.value_id == "NCBIGene:42"
+        assert gcontext.symbol == "MFG42"
+
+    def test_hgvs(self, mutation_factory: CdaMutationFactory):
         d = get_column_dict()
         d['Transcript_ID'] = "ENST00000380152"
         d['ENSP'] = "ENSP00000369497"
         d['HGVSc'] = "c.5526T>G"
         d['HGVSp'] = "p.Tyr1842Ter"
         series = pd.Series(d)
-        factory = CdaMutationFactory()
-        vinterpretation = factory.to_ga4gh(series)
 
-        self.assertIsNotNone(vinterpretation)
-        self.assertIsNotNone(vinterpretation.variation_descriptor)
-        self.assertEqual(2, len(vinterpretation.variation_descriptor.expressions))
+        vinterpretation = mutation_factory.to_ga4gh(series)
 
-        self.assertEqual("hgvs.c", vinterpretation.variation_descriptor.expressions[0].syntax)
-        self.assertEqual("ENST00000380152:c.5526T>G", vinterpretation.variation_descriptor.expressions[0].value)
+        assert vinterpretation is not None
+        assert vinterpretation.variation_descriptor is not None
+        assert len(vinterpretation.variation_descriptor.expressions) == 2
 
-        self.assertEqual("hgvs.p", vinterpretation.variation_descriptor.expressions[1].syntax)
-        self.assertEqual("ENSP00000369497:p.Tyr1842Ter", vinterpretation.variation_descriptor.expressions[1].value)
+        assert vinterpretation.variation_descriptor.expressions[0].syntax == "hgvs.c"
+        assert vinterpretation.variation_descriptor.expressions[0].value == "ENST00000380152:c.5526T>G"
 
-
-
-
-
-
-
+        assert vinterpretation.variation_descriptor.expressions[1].syntax == "hgvs.p"
+        assert vinterpretation.variation_descriptor.expressions[1].value == "ENSP00000369497:p.Tyr1842Ter"
