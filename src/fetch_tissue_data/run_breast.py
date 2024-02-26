4d2f984c3050e58b407426c16a41f889cb220c26
rajdeepmondal-el <rajdeep.mondal@elucidata.io>
2024-02-26 19:56:28 +0530

    uberon mappings and labels

--- src/fetch_tissue_data/run_breast.py
+++ src/fetch_tissue_data/run_breast.py
@@ -6,16 +6,18 @@ from oncoexporter.cda import CdaTableImporter, configure_cda_table_importer
 
 table_importer: CdaTableImporter = configure_cda_table_importer()
 
-Tsite = Q('primary_diagnosis_site = "%breast%"', )
-cohort_name = 'Breast'
-p = table_importer.get_ga4gh_phenopackets(Tsite, cohort_name=cohort_name)
+Tsite_breast = Q('primary_diagnosis_site = "%breast%"')
+cohort_name_breast = 'Breast'
+p_breast = table_importer.get_ga4gh_phenopackets(Tsite_breast, cohort_name=cohort_name_breast)
 
-result_dir = os.path.abspath(os.path.join('phenopackets', cohort_name))
-os.makedirs(result_dir, exist_ok=True)
 
-print(f'Writing {len(p)} phenopackets to {result_dir}')
-for pp in p:
-    file_path = os.path.join(result_dir, f'{pp.id}.json')
+result_dir_breast = os.path.abspath(os.path.join('phenopackets', cohort_name_breast))
+os.makedirs(result_dir_breast, exist_ok=True)
+
+# Writing phenopackets for Breast tissue
+print(f'Writing {len(p_breast)} phenopackets to {result_dir_breast}')
+for pp in p_breast:
+    file_path = os.path.join(result_dir_breast, f'{pp.id}.json')
     with open(file_path, 'w') as fh:
         json = MessageToJson(pp)
         fh.write(json)
