2dedb961751190cae526d0e1ce4e13a152c0e501
Justin Reese <justaddcoffee@gmail.com>
2024-04-23 11:13:14 -0400

    Update scripts to new CDA API syntax

--- scripts/run_bone.py
+++ scripts/run_bone.py
@@ -1,14 +1,17 @@
 import os
 from google.protobuf.json_format import MessageToJson
 
-from cdapython import Q
 from oncoexporter.cda import CdaTableImporter, configure_cda_table_importer
 
-table_importer: CdaTableImporter = configure_cda_table_importer()
+######   Input parameters  ########
+table_importer: CdaTableImporter = configure_cda_table_importer(use_cache=True)
 
-Tsite = Q('primary_diagnosis_site = "%bone%" OR primary_diagnosis_site = "%osseous%"', )
+Query = {'match_any': ['primary_diagnosis_site = *bone*',
+                       'primary_diagnosis_site = *osseous*'],
+         'data_source': 'GDC'}
 cohort_name = 'Bone'
-p = table_importer.get_ga4gh_phenopackets(Tsite, cohort_name=cohort_name)
+
+p = table_importer.get_ga4gh_phenopackets(Query, cohort_name=cohort_name)
 
 result_dir = os.path.abspath(os.path.join('phenopackets', cohort_name))
 os.makedirs(result_dir, exist_ok=True)
@@ -19,3 +22,4 @@ for pp in p:
     with open(file_path, 'w') as fh:
         json = MessageToJson(pp)
         fh.write(json)
+
