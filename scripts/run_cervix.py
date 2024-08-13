fa83feae3d3494203bebba6daedd22d413cf8a9d
Justin Reese <justaddcoffee@gmail.com>
2024-08-13 11:19:36 -0400

    Fix scripts to use new CDA API

--- scripts/run_cervix.py
+++ scripts/run_cervix.py
@@ -1,15 +1,27 @@
 import os
 from google.protobuf.json_format import MessageToJson
 
-from cdapython import Q
 from oncoexporter.cda import CdaTableImporter, configure_cda_table_importer
 
-table_importer: CdaTableImporter = configure_cda_table_importer()
+'''
+https://cda.readthedocs.io/en/latest/documentation/cdapython/code_update/#returning-a-matrix-of-results
+old: all of the functions previously used with, or chained onto Q()...run() have been replaced with the single function fetch_rows()
 
-Tsite = Q('primary_diagnosis_site = "%uter%" OR primary_diagnosis_site = "%cerv%"', )
+fetch_rows(table=None, *, match_all=[], match_any=[], data_source=[], add_columns=[], link_to_table='', provenance=False, count_only=False, return_data_as='dataframe', output_file='', debug=False)
+
+fetch_rows( table='subject', match_all=[ 'primary_disease_type = *duct*', 'sex = F*' ] )
+fetch_rows( table='researchsubject', match_all=[ 'primary_diagnosis_site = NULL' ] )
+'''
+######   Input parameters  ########
+table_importer: CdaTableImporter = configure_cda_table_importer(use_cache=True)
+
+Query = {'match_any': ['primary_diagnosis_site = *uter*',
+                       'primary_diagnosis_site = *cerv*'],
+         'data_source': 'GDC'}
 cohort_name = 'Cervix'
-p = table_importer.get_ga4gh_phenopackets(Tsite, cohort_name=cohort_name)
+####################################
 
+p = table_importer.get_ga4gh_phenopackets(Query, cohort_name=cohort_name)
 
 result_dir = os.path.abspath(os.path.join('phenopackets', cohort_name))
 os.makedirs(result_dir, exist_ok=True)
